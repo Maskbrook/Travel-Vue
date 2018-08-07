242b320535a134e6db6e6033a6a6d981ade91f37
zhangjiabao <zhangjiabao171110@credithc.com>
2018-08-07 19:13:23 +0800

    首页

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <div class="header-left"></div>
+    <div class="header-input"></div>
+    <div class="header-right"></div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+
+<!-- 使用stylus -->
+<!-- 1rem = (html的font-size) = 50px -->
+<style lang="stylus" scoped>
+  .header
+    height: 1rem
+    background: red
+</style>
