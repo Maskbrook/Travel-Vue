506b75ef43b935d509bc92ec6820f6181594e99f
zhangjiabao <zhangjiabao171110@credithc.com>
2018-08-14 11:28:16 +0800

    动态路由

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" />
+    <div class="banner-info">
+      <div class="banner-title">上海迪士尼乐园</div>
+      <div class="banner-num">15</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
