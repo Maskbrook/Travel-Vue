7c0b07bd690bc23a18006e8e83c12844c34241f8
zhangjiabao <zhangjiabao171110@credithc.com>
2018-08-14 14:10:41 +0800

    详情页banner

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="banner">
-    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" />
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" />
     <div class="banner-info">
       <div class="banner-title">上海迪士尼乐园</div>
-      <div class="banner-num">15</div>
+      <div class="banner-number">
+        <span class="iconfont back-icon">&#xe633;</span>
+        23
+      </div>
     </div>
   </div>
 </template>
@@ -15,4 +18,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        padding: 0 .15rem
+        font-size: .32rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        padding: 0 .15rem
+        margin-top: .14rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .back-icon
+          font-size: .24rem
 </style>
